5c548817494c58f8b59e0a433cc00d88c891347f
P Dalwadi <dalwad63@rowan.edu>
2026-01-11 22:57:45 -0500

    Adding to file

---
+++ main.py
@@ -0,0 +1,23 @@
+def add(num1, num2):
+    return num1 + num2
+print(add(15, 90))
+
+def subtract(num1, num2):
+    return num1 - num2
+print(subtract(95, 23))
+
+def multiply(num1, num2):
+    return num1 * num2
+print(multiply(20, 20))
+
+def double_division(num1, num2):
+    return num1 / num2
+print(double_division(45, 15))
+
+def int_division(num1, num2):
+    return num1 // num2
+print(int_division(45, 15))
+
+def remainder(num1, num2):
+    return num1 % num2
+print(remainder(90, 11))
